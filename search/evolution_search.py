e95ee66524e5b4192e9a2fe7c9e3b8365f047fee
Daniel Dimanov <i7461730@bournemouth.ac.uk>
2019-08-07 23:39:22 +0100

    Add train and validation paths and arguments

--- search/evolution_search.py
+++ search/evolution_search.py
@@ -7,6 +7,7 @@ import os
 import time
 import logging
 import argparse
+import torch
 from misc import utils
 
 import numpy as np
@@ -36,6 +37,11 @@ parser.add_argument('--n_offspring', type=int, default=40, help='number of offsp
 parser.add_argument('--init_channels', type=int, default=24, help='# of filters for first cell')
 parser.add_argument('--layers', type=int, default=11, help='equivalent with N = 3')
 parser.add_argument('--epochs', type=int, default=25, help='# of epochs to train during architecture search')
+#TODO document ADD data dir and labels file
+parser.add_argument('--train_data_directory', type=str, default='/content/dataFinal/train', help='Directory containing train images')
+parser.add_argument('--validation_data_directory', type=str, default='/content/dataFinal/validation', help='Directory containing validation images')
+parser.add_argument('--labels_file_path', type=str, default='/content/gdrive/My Drive/datasetTrain/labelsFin.txt', help='Directory containing validation images')
+
 args = parser.parse_args()
 args.save = 'search-{}-{}-{}'.format(args.save, args.search_space, time.strftime("%Y%m%d-%H%M%S"))
 utils.create_exp_dir(args.save)
@@ -49,6 +55,61 @@ logging.getLogger().addHandler(fh)
 
 pop_hist = []  # keep track of every evaluated architecture
 
+
+# ---------------------------------------------------------------------------------------------------------
+# GET DATASET
+# ---------------------------------------------------------------------------------------------------------
+from PIL import Image
+import pathlib
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from keras.preprocessing import image
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+
+folders ={"train":args.train_data_directory, "validation":args.validation_data_directory}
+lblSFile=args.labels_file_path
+file = open(lblSFile, "r")
+imCLs=[]
+for imCL in file:
+  imCLs.append(str(imCL)[:-1])
+X_train,Y_train,X_val,Y_val=[],[],[],[]
+for step in ['train','validation']:
+    X,Y=[],[]
+    for cl in imCLs:
+        directory=''+folders[step]+"/"+str(cl)
+        print("Getting images from:"+directory)
+        size=512,512
+        print("Processing class "+ str(cl))
+        for filename in os.listdir(directory):
+            if(filename.startswith('.')):
+                continue
+            else:
+                img = image.load_img(directory+'/'+filename,target_size=(128,128,3),grayscale=False)
+                img = image.img_to_array(img)
+                img = img/255
+                X.append(img)
+                lbl=str(cl)
+                Y.append(imCLs.index(lbl))
+    if(step=='train'):
+        X_train = np.array(X)
+        Y_train = np.array(Y)
+    if(step=='validation'):
+        X_val = np.array(X)
+        Y_val = np.array(Y)
+    del X
+    del Y
+X_train_swap = np.swapaxes(X_train,-1,1)
+X_val_swap = np.swapaxes(X_val,-1,1)
+train_dataset = data.TensorDataset(torch.from_numpy(X_train_swap),torch.from_numpy(Y_train))
+val_dataset = data.TensorDataset(torch.from_numpy(X_val_swap),torch.from_numpy(Y_val))
+del X_train,Y_train,X_train_swap,X_val_swap
 
 # ---------------------------------------------------------------------------------------------------------
 # Define your NAS Problem
@@ -87,7 +148,9 @@ class NAS(Problem):
                                             layers=self._layers, cutout=False,
                                             epochs=self._epochs,
                                             save='arch_{}'.format(arch_id),
-                                            expr_root=self._save_dir)
+                                            expr_root=self._save_dir,
+                                            train_dataset=train_dataset,
+                                            val_dataset=val_dataset)
 
             # all objectives assume to be MINIMIZED !!!!!
             objs[i, 0] = 100 - performance['valid_acc']
